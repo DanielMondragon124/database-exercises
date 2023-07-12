0c46f7eb8d69cfe87f0ceb47ac28d4a3941baf23
daniel mondragon <daniel.mondragon004@GMAIL.COM>
2023-07-12 09:35:42 -0500

    update

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50),
                         name  VARCHAR(100) NOT NULL,
-                        release_date TEXT INT NOT NULL,
+                        release_date INT NOT NULL,
                         sales FLOAT NOT NULL,
                         genre VARCHAR(50),
                         PRIMARY KEY (id)
